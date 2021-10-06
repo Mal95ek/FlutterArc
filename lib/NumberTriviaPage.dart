c915f088f75cd272213b411ba728342926dfed52
Mal95ek <70469305+Mal95ek@users.noreply.github.com>
2021-10-06 15:49:04 +0300

    Delete lib directory

--- lib/NumberTriviaPage.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-class NumberTriviaPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
--- lib/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++
@@ -1,6 +0,0 @@
-
-import 'package:clean_architecture_tdd_course/features/number_trivia/domain/entints/number_trivia.dart';
-abstract class NumberTriviaRemoteDataSource {
-  Future<NumberTrivia> getConcreateNumberTrivia(int number);
-  Future<NumberTrivia> getRandomNumberTrivia();
-}
